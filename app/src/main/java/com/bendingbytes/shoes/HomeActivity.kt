d7ebd1cf7ba90a19e4cc5e59e919e6dffa548983
BendingBytes <>
2022-06-03 12:31:28 +0200

    ## Information Task BBM23 Add bottom navigation to HomeActivity Added a bottom navigation ##Changes Added nav_menu Added favourite and list fragments Added bottom navigation view to Home activity ##How to test Run the app observe if animation is running observe if splash activity is finished after 3 seconds and moved to Home

--- app/src/main/java/com/bendingbytes/shoes/HomeActivity.kt
+++ app/src/main/java/com/bendingbytes/shoes/HomeActivity.kt
@@ -2,10 +2,61 @@ package com.bendingbytes.shoes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.bendingbytes.shoes.Fragments.FavouritesFragment
+import com.bendingbytes.shoes.Fragments.ListFragment
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        Log.d("HomeActivity", "Bbytes onCreate")
+
+
+        //bottom navigation
+        val favouritesFragment= FavouritesFragment()
+        val listFragment= ListFragment()
+        makeCurrentFragment(listFragment)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_baseline_format_list_numbered -> makeCurrentFragment (listFragment)
+                R.id.ic_baseline_star -> makeCurrentFragment (favouritesFragment)
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.bottom_navigation, fragment)
+            commit()
+        }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("HomeActivity", "Bbytes onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("HomeActivity", "Bbytes onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("HomeActivity", "Bbytes onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("HomeActivity", "Bbytes onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("HomeActivity", "Bbytes onDestroy")
     }
 }
--- app/src/main/java/com/bendingbytes/shoes/SplashActivity.kt
+++ app/src/main/java/com/bendingbytes/shoes/SplashActivity.kt
@@ -4,13 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.bendingbytes.shoes.Fragments.FavouritesFragment
+import com.bendingbytes.shoes.Fragments.ListFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         animationView.postDelayed(Runnable { moveToHome() }, 3000)
+        Log.d("SplashActivity", "Bbytes onCreate")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("SplashActivity", "Bbytes onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("SplashActivity", "Bbytes onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("SplashActivity", "Bbytes onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("SplashActivity", "Bbytes onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SplashActivity", "Bbytes onDestroy")
     }
 
     private fun moveToHome() {
