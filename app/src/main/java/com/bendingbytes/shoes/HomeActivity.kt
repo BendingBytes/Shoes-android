7207e7e04213efe115ee8f9a5cd2af16289129c0
BendingBytes <>
2022-06-03 14:00:26 +0200

    Made UI changes to improve layouts

--- app/src/main/java/com/bendingbytes/shoes/HomeActivity.kt
+++ app/src/main/java/com/bendingbytes/shoes/HomeActivity.kt
@@ -7,5 +7,26 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        Log.d("HomeActivity", "Bbytes onCreate")
+
+
+        //bottom navigation
+        val favouritesFragment = FavouritesFragment()
+        val listFragment = ListFragment()
+        makeCurrentFragment(listFragment)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_baseline_format_list_numbered -> makeCurrentFragment(listFragment)
+                R.id.ic_baseline_star -> makeCurrentFragment(favouritesFragment)
+            }
+            true
+        }
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.constraintHome, fragment)
+            commit()
+        }
+
 }
