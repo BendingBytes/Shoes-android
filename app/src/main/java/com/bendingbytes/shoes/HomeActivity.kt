fd1aadc1aad1a9838e882bded9e9e7414362efa4
BendingBytes <>
2022-06-03 14:27:33 +0200

    implemented splash screen with custom theme

---
+++ app/src/main/java/com/bendingbytes/shoes/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.bendingbytes.shoes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
