17a5e51eed66e0c9061f9506895b9f655460ce41
Bosko <>
2022-06-10 13:30:42 +0200

    Created fragments

--- app/src/main/java/com/bendingbytes/shoes/HomeActivity.kt
+++ app/src/main/java/com/bendingbytes/shoes/HomeActivity.kt
@@ -2,6 +2,10 @@ package com.bendingbytes.shoes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.ListFragment
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
