26eb493fb8cfd2c338779d597823409c446cc8b6
Bosko <>
2022-06-10 14:07:30 +0200

    Created ListFragments

--- app/src/main/java/com/bendingbytes/shoes/HomeActivity.kt
+++ app/src/main/java/com/bendingbytes/shoes/HomeActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.ListFragment
-import com.bendingbytes.shoes.Fragments.FavouritesFragment
+import com.bendingbytes.shoes.fragments.FavouritesFragment
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
--- app/src/main/java/com/bendingbytes/shoes/fragments/ListFragments.kt
+++ app/src/main/java/com/bendingbytes/shoes/fragments/ListFragments.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.ListFragment
 import com.bendingbytes.shoes.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ListFragment.newInstance] factory method to
+ * Use the [FavouritesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListFragment : Fragment() {
+class ListFragments : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +36,7 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return inflater.inflate(R.layout.fragment_favourites, container, false)
     }
 
     companion object {
@@ -45,7 +46,7 @@ class ListFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ListFragment.
+         * @return A new instance of fragment FavouritesFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
