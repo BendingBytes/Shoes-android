09243e201928b9b96b579875ac519488fe9c94b9
Bosko <>
2022-06-14 10:53:21 +0200

    Implemented timber lib and made some class that include timber

---
+++ app/src/main/java/com/bendingbytes/shoes/ShoeApplication.kt
@@ -0,0 +1,15 @@
+package com.bendingbytes.shoes
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+
+class ShoeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
