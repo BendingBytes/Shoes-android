31528cc4807dc68ab36b89f29fcd467c09e040e9
Bosko <>
2022-06-15 10:29:13 +0200

    Deleted params and fields, changed function loadShoes

--- app/src/main/java/com/bendingbytes/shoes/view/ShoeViewModel.kt
+++ app/src/main/java/com/bendingbytes/shoes/view/ShoeViewModel.kt
@@ -1,43 +1,40 @@
 package com.bendingbytes.shoes.view
 
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bendingbytes.shoes.domain.DataState
 import com.bendingbytes.shoes.domain.Shoe
-import com.bendingbytes.shoes.room.ShoeCacheMapper
-import com.bendingbytes.shoes.room.ShoeDao
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import com.bendingbytes.shoes.repository.ShoesRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class ShoeViewModel(
-    private val shoeCacheMapper: ShoeCacheMapper,
-    private val shoeDao: ShoeDao,
-    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+    private val shoesRepository: ShoesRepository,
 ) : ViewModel() {
-    private val shoes: MutableLiveData<List<Shoe>> by lazy {
-        MutableLiveData<List<Shoe>>().also {
-            loadShoes(shoeCacheMapper, shoeDao, ioDispatcher)
+    private val mutableShoeLiveDataState: MutableLiveData<DataState<List<Shoe>>> =
+        MutableLiveData()
+
+
+    @ExperimentalCoroutinesApi
+    fun loadShoes(forceFetch: Boolean = false) {
+        viewModelScope.launch {
+            if (forceFetch) {
+                shoesRepository.getShoes()
+                    .onEach { dataState ->
+                        mutableShoeLiveDataState.postValue(dataState)
+                    }.catch {
+                        Timber.e(it)
+                        loadShoes(false) // fallback: load from DB
+                    }.launchIn(this)
+            } else {
+                Timber.e("Error")
+            }
         }
-    }
-
-    private fun loadShoes(
-        shoeCacheMapper: ShoeCacheMapper,
-        shoeDao: ShoeDao,
-        ioDispatcher: CoroutineDispatcher = Dispatchers.IO
-    ): Flow<DataState<List<Shoe>>> = flow {
-        val shoesCacheEntities = shoeDao.getAll()
-        val shoesDb = shoeCacheMapper.mapFromListEntity(shoesCacheEntities)
-        emit(DataState.Success(shoesDb))
-        Timber.i("Shoes are loaded")
-    }.flowOn(ioDispatcher)
-
-    fun getShoes(): LiveData<List<Shoe>> {
-        return shoes
     }
 }
