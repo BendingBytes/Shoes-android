bb4a1a4d7334703b79fe1505016876ed12007a89
Bosko <>
2022-06-15 09:40:57 +0200

    Deleted some unused imports

--- app/src/main/java/com/bendingbytes/shoes/fragments/ListFragments.kt
+++ app/src/main/java/com/bendingbytes/shoes/fragments/ListFragments.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
 import com.bendingbytes.shoes.R
-import com.bendingbytes.shoes.view.ShoeViewModel
 
 
 class ListFragments : Fragment() {
