70aabbcffb1f47fe641feb7b5ce07b08831c316c
Uros Simic <UkiUros@users.noreply.github.com>
2022-06-16 10:40:33 +0200

    Revert "Created some dependencies, created onViewCreated methods in fragments…"

--- app/src/main/java/com/bendingbytes/shoes/fragments/ListFragments.kt
+++ app/src/main/java/com/bendingbytes/shoes/fragments/ListFragments.kt
@@ -13,7 +13,6 @@ class ListFragments : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_list, container, false)
-
     companion object {
         fun newInstance() = ListFragments()
     }
